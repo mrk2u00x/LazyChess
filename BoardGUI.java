59f9f1b7b1889004931296b244c5ac6be53b3cf5
BryanL530 <bryanlin530@gmail.com>
2022-03-04 17:18:49 -0800

    Button grid added
    Using button grid as the way to understand where the user clicked on.
    
    WIP
    Turning the grid invisible for user to see the board
    Implementing movement code into grid

--- BoardGUI.java
+++ BoardGUI.java
@@ -2,24 +2,38 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.Toolkit;
+import java.awt.event.MouseEvent;
+import java.awt.event.ActionEvent;
+import java.awt.Point;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+import javax.swing.Timer;
 import javax.swing.border.EmptyBorder;
+import javax.swing.event.MouseInputListener;
 import javax.swing.JLabel;
+import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 
-public class BoardGUI extends JFrame {
+public class BoardGUI extends JFrame
+{
 
 	private JPanel contentPane;
 	private Logic gameBoard;
+	private Point selectedStart;
+	private Point selecctedEnd;
+
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args)
 	{
-		EventQueue.invokeLater(() -> run());	
+		new BoardGUI();
+		//EventQueue.invokeLater(() -> run());	
 	}
 	
 	public static void run()
@@ -44,8 +58,8 @@ public class BoardGUI extends JFrame {
 		for(int y = 0; y < 4; y++)
 			for(int x = 0; x < 4; x++)
 			{
-				int baseX = 150 * x + 8;
-				int baseY = 150 * y + 30;
+				int baseX = 150 * x + 15;
+				int baseY = 150 * y + 50;
 				
 				g.setColor(Color.WHITE);
 				g.fillRect(baseX, baseY, 75, 75);
@@ -71,24 +85,54 @@ public class BoardGUI extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBackground(Color.gray);
 		setResizable(false);
-		setSize(616, 638);
+		setSize(640, 750);
 		setTitle("Lazy Chess Game");
 		setIconImage(Toolkit.getDefaultToolkit().getImage(About.class.getResource("/assets/white_king.png")));
 		setLocationRelativeTo(null);
-		setVisible(true);
-
+		setLayout(null);
+		
+/*
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(getLayout());
 		contentPane.setLayout(null);
-
-
+*/
 		gameBoard = new Logic(name1, name2);
+
+		initializeButtons();
+		//Image a = Toolkit.getDefaultToolkit().getImage(BoardGUI.class.getResource("assets/black_bishop.png");
+		//gridButtons[0][0].setIcon(new ImageIcon(a));
 		
-//		JLabel lblNewLabel = new JLabel("");
-//		lblNewLabel.setIcon(new ImageIcon(BoardGUI.class.getResource("/assets/white_castle.png")));
-//		lblNewLabel.setBounds(10, 540, 118, 110);
-//		contentPane.add(lblNewLabel);
+		setVisible(true);
+	}
+
+	public void selectedPoint(Point p)
+	{
+		
+		System.out.println("setected point : " + p);
+		//if(gameBoard.legalSelect(p))
+	}
+
+	public void initializeButtons()
+	{
+			
+		JButton[][] gridButtons = new JButton[8][8];
+		
+		for(int y = 0; y < 8; y++)
+			for(int x = 0; x < 8; x++)
+			{	
+				final int pX = x;
+				final int pY = 8 - y;
+
+				gridButtons[x][y] = new JButton();
+				gridButtons[x][y].setBounds(75 * x + 10, 75 * y + 30, 75, 75);
+				gridButtons[x][y].addActionListener(e -> selectedPoint(new Point(pX, pY)));
+				gridButtons[x][y].setOpaque(false);
+				gridButtons[x][y].setContentAreaFilled(false);
+				gridButtons[x][y].setBorderPainted(false);
+				
+				getContentPane().add(gridButtons[x][y]);
+			}
 	}
 }
